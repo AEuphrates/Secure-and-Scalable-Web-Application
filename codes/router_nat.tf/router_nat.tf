e5a0502a19b2b6c371660e6d11f2d36004a96d06
Ali Fırat Çobanoğlu <155168172+AEuphrates@users.noreply.github.com>
2024-03-13 16:29:27 +0300

    Update router_nat.tf

--- codes/router_nat.tf/router_nat.tf
+++ codes/router_nat.tf/router_nat.tf
@@ -1,1 +1,13 @@
+resource "google_compute_router" "cloud_router" {
+    name ="my-cloud-router"
+    network = google_compute_network.vpc_network.self_link
+    
+}
 
+resource "google_compute_router_nat" "nat_gateway" {
+    name= "my-nat-gateway"
+    router = google_compute_router.cloud_router.name
+    region = "europe-west1"
+    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+      nat_ip_allocate_option = "AUTO_ONLY"
+}
